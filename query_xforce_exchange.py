42afc0cc251c5644bbc33ff617f0ce6c052c9a71
Noah Clements <noahnoah900@gmail.com>
2019-01-26 17:35:35 -0400

    loops through IP's and determines riskiness
    if the risk score of the IP is >4 on X-Force Exchange, it will print.
    python 2.7

--- query_xforce_exchange.py
+++ query_xforce_exchange.py
@@ -10,8 +10,14 @@ def send_request(apiurl, scanurl, headers):
     fullurl = apiurl +  scanurl
     response = requests.get(fullurl, params='', headers=headers, timeout=20)
     all_json = response.json()
-    print all_json['ip']
-    print all_json['score']
+    score = int(all_json['score'])
+    if score > 4:
+    	ip = all_json['ip']
+    	print ip
+    	print score
+    else:
+    	print "not a threat"
+
 
 
 key = "<api key here>" 
@@ -24,5 +30,5 @@ XForce_url = "https://api.xforce.ibmcloud.com:443"
 URL_File = open('IPs.txt', 'r')
 
 for url in URL_File:
-    apiurl = XForce_url + "/ipr/"
-    send_request(apiurl, url, headers)
+	apiurl = XForce_url + "/ipr/"
+	send_request(apiurl, url, headers)
