40097d393c6e9542b8f90b2869d6e40fae926bcc
Noah Clements <noahnoah900@gmail.com>
2019-01-25 23:23:39 -0400

    stripped down to basics
    Next to implement: reading from file

--- query_xforce_exchange.py
+++ query_xforce_exchange.py
@@ -12,23 +12,15 @@ def send_request(apiurl, scanurl, headers):
     all_json = response.json()
     print json.dumps(all_json, indent=4, sort_keys=True) 
 
-def get_md5(filename):
-    try:
-        f = open(filename,"rb")
-        md5 = hashlib.md5((f).read()).hexdigest()
-        return md5
-    except e:
-        print str(e)
 
-if __name__ == "__main__":
-    key = "<api_key_here>" 
-    password ="<password_here>"
+key = "<api key here>" 
+password ="<password here>"
 
-    token = base64.b64encode(key + ":" + password)
-    headers = {'Authorization': "Basic " + token, 'Accept': 'application/json'}
-    url = "https://api.xforce.ibmcloud.com:443"
+token = base64.b64encode(key + ":" + password)
+headers = {'Authorization': "Basic " + token, 'Accept': 'application/json'}
+url = "https://api.xforce.ibmcloud.com:443"
  
-    scanurl = raw_input("Enter an IP Address to scan")
+scanurl = raw_input("Enter an IP Address to scan")
 
-    apiurl = url + "/ipr/"
-    send_request(apiurl, scanurl, headers)
+apiurl = url + "/ipr/"
+send_request(apiurl, scanurl, headers)
