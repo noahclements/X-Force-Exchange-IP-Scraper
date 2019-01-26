e13f5320b17eff2a75ca6ab343e2f15e6a70413d
Noah Clements <noahnoah900@gmail.com>
2019-01-25 23:04:44 -0400

    Just does IP scanning now with input from user

--- query_xforce_exchange.py
+++ query_xforce_exchange.py
@@ -22,61 +22,13 @@ def get_md5(filename):
 
 if __name__ == "__main__":
     key = "<api_key_here>" 
-    password ="password_here>"
+    password ="<password_here>"
 
     token = base64.b64encode(key + ":" + password)
     headers = {'Authorization': "Basic " + token, 'Accept': 'application/json'}
     url = "https://api.xforce.ibmcloud.com:443"
+ 
+    scanurl = raw_input("Enter an IP Address to scan")
 
-
-    parser = OptionParser()
-    
-    parser.add_option("-i", "--ip", dest="s_ip" , default=None,
-                      help="ip to be checked", metavar="ipaddress")
-(options, args) = parser.parse_args()
-
-
-if (options.s_ip is not None):
-    scanurl = options.s_ip
     apiurl = url + "/ipr/"
     send_request(apiurl, scanurl, headers)
-    apiurl = url + "/ipr/history/"
-    send_request(apiurl, scanurl, headers)
-    apiurl = url + "/ipr/malware/"
-    send_request(apiurl, scanurl, headers)
-
-
-if len(sys.argv[1:]) == 0:
-  parser.print_help()
-
-if ( options.s_url is not None ):
-    apiurl = url + "/url/"
-    scanurl = options.s_url
-    send_request(apiurl, scanurl, headers)
-elif ( options.m_url is not None ):
-    apiurl = url + "/url/malware/" 
-    scanurl = options.m_url
-    send_request(apiurl, scanurl, headers)
-elif ( options.s_cve is not None ):
-    apiurl = url + "/vulnerabilities/search/" 
-    scanurl = options.s_cve
-    send_request(apiurl, scanurl, headers)
-elif (options.s_ip is not None):
-    scanurl = options.s_ip
-    apiurl = url + "/ipr/"
-    send_request(apiurl, scanurl, headers)
-    apiurl = url + "/ipr/history/"
-    send_request(apiurl, scanurl, headers)
-    apiurl = url + "/ipr/malware/"
-    send_request(apiurl, scanurl, headers)
-elif (options.malfile is not None ):
-    md5 = get_md5(options.malfile)
-    if md5:
-        send_request(url+"/malware/", md5, headers)
-elif (options.s_xfid is not None ):
-    send_request(url+"/vulnerabilities/", options.s_xfid, headers)
-elif (options.hash is not None ):
-    send_request(url+"/malware/", options.hash, headers)
-
-if len(sys.argv[1:]) == 0:
-    parser.print_help()
