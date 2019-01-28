942aafda5b02a2959b96f43cb6df02f05565d3ad
Noah Clements <noahnoah900@gmail.com>
2019-01-28 16:33:23 -0400

    fixed "unauthorized" error when calling API
    Reason: the token was a binary string and was sent as: 
    b'<token here>' instead of just
    <token here>
    script now works on python 3.7

--- XForceScript3_7.py
+++ XForceScript3_7.py
@@ -10,7 +10,6 @@ def send_request(apiurl, scanurl, headers):
     fullurl = apiurl +  scanurl
     response = requests.get(fullurl, params='', headers=headers, timeout=20)
     all_json = response.json()
-    print(all_json)
     score = int(all_json['score'])
     if score > 4:
     	ip = all_json['ip']
@@ -21,15 +20,13 @@ def send_request(apiurl, scanurl, headers):
 
 
 
-key = 'b08397d1-3d25-4458-94cd-1966da1afca2'
-password = 'ac6bcb2f-ef8e-4ec5-a1a2-705f641bf720'
+key = '<api key here>'
+password = '<password here>'
 
 data_string = key + ":" + password
 data_bytes = data_string.encode('utf-8')
 
-#test = '%s:%s' % (key,password)
-token = str(base64.b64encode(data_bytes))
-
+token = base64.b64encode(data_bytes).decode('ascii')
 headers = {'Authorization': "Basic " + token, 'Accept': 'application/json'}
 XForce_url = "https://api.xforce.ibmcloud.com:443"
  
@@ -38,4 +35,5 @@ URL_File = open('IPs.txt', 'r')
 for url in URL_File:
 	apiurl = XForce_url + "/ipr/"
 	send_request(apiurl, url, headers)
+
 
