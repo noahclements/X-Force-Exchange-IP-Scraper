4e8de8f209c2675fa2d2e748db8e3f6a9ed1827a
Noah Clements <noahnoah900@gmail.com>
2019-01-28 16:43:57 -0400

    moved to main script

--- Cb_IP_Getter.py
+++
@@ -1,8 +0,0 @@
-import re
-
-ip = re.findall( r'[0-9]+(?:\.[0-9]+){3}', open('rawIPs.txt', 'r').read())
-
-file = open('cleanIPs.txt', 'w')
-for line in ip:
-    file.write(line + '\n')
-file.close()
