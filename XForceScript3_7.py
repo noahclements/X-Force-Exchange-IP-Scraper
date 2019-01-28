0579ba619af7df8bcbbb371100aa4d0865f9f732
Noah Clements <noahnoah900@gmail.com>
2019-01-28 16:43:10 -0400

    added the parser script
    parser takes raw data from a site that has IP's in them, and outputs just the IP's into a text file.
    Next will probably be just to put them into an array, as writing to a text file seems redundant.

--- XForceScript3_7.py
+++ XForceScript3_7.py
@@ -5,6 +5,7 @@ import json
 from optparse import OptionParser
 import hashlib
 import base64
+import re
 
 def send_request(apiurl, scanurl, headers):
     fullurl = apiurl +  scanurl
@@ -18,10 +19,17 @@ def send_request(apiurl, scanurl, headers):
     else:
     	print("not a threat")
 
+# this copies in the raw data from site, and output just the IPs
+findIP = re.findall( r'[0-9]+(?:\.[0-9]+){3}', open('rawIPs.txt', 'r').read())
+
+file = open('IPs.txt', 'w')
+for line in findIP:
+    file.write(line + '\n')
+file.close()
 
 
-key = '<api key here>'
-password = '<password here>'
+key = '6c270482-aa75-46b6-a65b-3e94379dedae'
+password = 'db62ca96-015c-4f26-a34c-54e43f4b2301'
 
 data_string = key + ":" + password
 data_bytes = data_string.encode('utf-8')
@@ -29,11 +37,9 @@ data_bytes = data_string.encode('utf-8')
 token = base64.b64encode(data_bytes).decode('ascii')
 headers = {'Authorization': "Basic " + token, 'Accept': 'application/json'}
 XForce_url = "https://api.xforce.ibmcloud.com:443"
- 
+
 URL_File = open('IPs.txt', 'r')
 
 for url in URL_File:
 	apiurl = XForce_url + "/ipr/"
 	send_request(apiurl, url, headers)
-
-
