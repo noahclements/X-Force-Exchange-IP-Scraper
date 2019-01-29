8aa6f007b16ef3c8ea53135522514f1a4cf3a3e5
Noah Clements <noahnoah900@gmail.com>
2019-01-29 16:15:11 -0400

    places IP's into an array instead of text file
    also removed some unnecessary packages

--- XForceScript3_7.py
+++ XForceScript3_7.py
@@ -1,9 +1,5 @@
 import requests
-import urllib3
-import sys
 import json
-from optparse import OptionParser
-import hashlib
 import base64
 import re
 
@@ -12,24 +8,25 @@ def send_request(apiurl, scanurl, headers):
     response = requests.get(fullurl, params='', headers=headers, timeout=20)
     all_json = response.json()
     score = int(all_json['score'])
+    print(score)
     if score > 4:
-    	ip = all_json['ip']
-    	print(ip)
-    	print(score)
+        ip = all_json['ip']
+        print(ip)
+        print(score)
     else:
-    	print("not a threat")
+        print("not a threat")
 
 # this copies in the raw data from site, and output just the IPs
-findIP = re.findall( r'[0-9]+(?:\.[0-9]+){3}', open('rawIPs.txt', 'r').read())
+findIP = re.findall(r'[0-9]+(?:\.[0-9]+){3}', open('rawIPs.txt', 'r').read())
 
-file = open('IPs.txt', 'w')
+ipList = []
 for line in findIP:
-    file.write(line + '\n')
-file.close()
+   ipList.append(line)
 
 
-key = '6c270482-aa75-46b6-a65b-3e94379dedae'
-password = 'db62ca96-015c-4f26-a34c-54e43f4b2301'
+
+key = '<api key here>'
+password = '<password here>'
 
 data_string = key + ":" + password
 data_bytes = data_string.encode('utf-8')
@@ -38,8 +35,7 @@ token = base64.b64encode(data_bytes).decode('ascii')
 headers = {'Authorization': "Basic " + token, 'Accept': 'application/json'}
 XForce_url = "https://api.xforce.ibmcloud.com:443"
 
-URL_File = open('IPs.txt', 'r')
 
-for url in URL_File:
-	apiurl = XForce_url + "/ipr/"
-	send_request(apiurl, url, headers)
+for url in ipList:
+    apiurl = XForce_url + "/ipr/"
+    send_request(apiurl, url, headers)
